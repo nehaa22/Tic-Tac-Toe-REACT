cf100850508c55dfdd7b2ccf657c5dec7b3650f9
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-05-05 12:30:59 +0530

    Refactor. removed multiple div for box using Array and map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      board : Array(9).fill("x")
+    }
+  }
+  
+
+render(){
+
+const Box = this.state.board.map(box => <div className = "box">{box}</div> )
+
   return (
     <div className="container">
       <h1> Tic-Tac-Toe</h1>
       <div className = "board">
+        {Box}
+      {/* <div className = "box"></div>
       <div className = "box"></div>
       <div className = "box"></div>
       <div className = "box"></div>
@@ -13,13 +28,13 @@ function App() {
       <div className = "box"></div>
       <div className = "box"></div>
       <div className = "box"></div>
-      <div className = "box"></div>
-      <div className = "box"></div>
+      <div className = "box"></div> */}
       </div>
 
 
     </div>
   );
+}
 }
 
 export default App;
