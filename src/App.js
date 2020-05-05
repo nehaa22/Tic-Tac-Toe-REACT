7f0d8e54c090e638a39ddb7f88737c5c8b06e2da
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-05-05 12:12:31 +0530

    Add. add board with 9 blocks

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1> Tic-Tac-Toe</h1>
+      <div className = "board">
+      <div className = "box"></div>
+      <div className = "box"></div>
+      <div className = "box"></div>
+      <div className = "box"></div>
+      <div className = "box"></div>
+      <div className = "box"></div>
+      <div className = "box"></div>
+      <div className = "box"></div>
+      <div className = "box"></div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
